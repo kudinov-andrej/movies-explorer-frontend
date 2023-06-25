83c0185e3dd0e774b8633915ff156a1dc0ad8596
Andrej Kudinov <kudinov.andrej@list.ru>
2023-06-25 13:28:36 +0300

    сделал верстку блока project-title

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -18,7 +18,20 @@ function App() {
       <Header
         handleСhangePopapNavBar={handleСhangePopapNavBar}
         isOpenPopapNavBar={isOpenPopapNavBar}
-        islogin={islogin} />
+        islogin={islogin}
+      />
+      <main className='content'>
+        <section className='project-title'>
+          <h2 className='project-title__title'>Учебный проект студента факультета Веб-разработки.</h2>
+          <nav className='project-title__navigation'>
+            <ul className='project-title__navigation-list'>
+              <li className='project-title__navigation-item'><button className='project-title__button'>О проекте</button></li>
+              <li className='project-title__navigation-item'><button className='project-title__button'>Технологии</button></li>
+              <li className='project-title__navigation-item'><button className='project-title__button'>Студент</button></li>
+            </ul>
+          </nav>
+        </section>
+      </main>
     </body>
   );
 }
